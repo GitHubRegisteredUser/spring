313caf5c98ddeb77ca951a1f9007458fdc08e2d6
GitHubRegisteredUser <131602241+GitHubRegisteredUser@users.noreply.github.com>
2024-01-11 19:28:39 +0300

    Initial commit

---
+++ src/main/java/crud/service/UserServiceImp.java
@@ -0,0 +1,47 @@
+package crud.service;
+
+import crud.dao.UserDao;
+import crud.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class UserServiceImp implements UserService {
+
+    @Autowired
+    UserDao userDao;
+
+    @Override
+    @Transactional
+    public List<User> getAllUsers() {
+        return userDao.getAllUsers();
+    }
+
+    @Override
+    @Transactional
+    public void saveUser(User user) {
+        userDao.saveUser(user);
+    }
+
+    @Override
+    @Transactional
+    public User getUser(long id) {
+        return userDao.getUser(id);
+    }
+
+    @Override
+    @Transactional
+    public void updateUser(User user) {
+        userDao.updateUser(user);
+    }
+
+    @Override
+    @Transactional
+    public void deleteUser(long id) {
+        userDao.deleteUser(id);
+    }
+
+}
